8e67ad2f8d5878fd229a289c8bf9fd59566bc54e
Nam Nguyen <nguyentnam.061297@gmail.com>
2023-08-27 13:15:43 +0700

    1

--- src/lab06/Main.java
+++ src/lab06/Main.java
@@ -1,5 +1,6 @@
 package lab06;
 
+import lab06.sale.TableList;
 import lab06.sale.entities.Product;
 import lab06.sale.repositories.Singleton;
 
@@ -9,6 +10,12 @@ public class Main {
         var pro = Singleton.products.stream().filter(x -> x.getName().equals("Product1")).findAny().orElse(new Product());
         pro.setName("BAU DUOI");
         Singleton.saveProduct(pro);
+
+        TableList tl = new TableList(3, "ID", "NAME", "DES").sortBy(0).withUnicode(true);
+// from a list
+        a.forEach(element -> tl.addRow(String.valueOf(element.getId()), element.getName(), element.getDes()));
+// or manually
+        tl.print();
         System.out.println("abc");
     }
 }
